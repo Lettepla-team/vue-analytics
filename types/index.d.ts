8171e064dbffbd02504ac87bd8a83b030ec7ace1
Andréas HANSS <ahanss@kaliop.com>
2018-02-19 11:34:14 +0100

    Update build

---
+++ types/index.d.ts
@@ -0,0 +1,8 @@
+import "./vue";
+import { VueAnalytics } from "./ua";
+
+export default VueAnalytics;
+
+export {
+  VueAnalyticsOptions
+} from "./ua";
---
+++ types/ua.d.ts
@@ -0,0 +1,34 @@
+import VueRouter from 'vue-router'
+
+export declare class VueAnalytics {
+  constructor (options?: VueAnalyticsOptions);
+
+  trackView (name: String, trackPage?: boolean): void;
+  trackEvent (category: String, action?: String, label?: String, value?: any): void;
+  trackException (description: String, isFatal?: boolean);
+  trackTiming (timingCategory: String, timingVar: String, timingValue: number, timingLabel?: String): void;
+  injectGlobalDimension (dimensionNumber: number, value: String|number): void;
+  injectGlobalMetric (metricNumber: number, value: String|number): void;
+  changeSessionLanguage (code: String): void;
+}
+
+export declare class VueAnalyticsOptions {
+  appName: String;
+  appVersion: String;
+  trackingId: String;
+  vueRouter: VueRouter;
+  ignoredViews: String[];
+  trackPage: boolean;
+  globalDimensions?: Dimension[];
+  globalMetrics?: Metric[];
+}
+
+export type Dimension = {
+  dimension: number;
+  value: String;
+}
+
+export type Metric = {
+  metric: number;
+  value: String;
+}
